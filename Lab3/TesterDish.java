4a53f5dbdadf8653db6d03d2831e48324dd6e702
islam1999 <31537821+islam1999@users.noreply.github.com>
2018-10-21 16:17:35 +0300

    Add files via upload

---
+++ Lab3/TesterDish.java
@@ -0,0 +1,8 @@
+package ru.mirea.Lab3;
+
+public class TesterDish {
+    public static void main(String[] args) {
+        Tarelka t = new Tarelka("Керамическая", "Белый", "Большая");
+    System.out.println(t);
+    }
+}
