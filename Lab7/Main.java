304a7b92c4f6fb5ad9b0060bbe64b95847f9399f
islam1999 <31537821+islam1999@users.noreply.github.com>
2018-11-19 11:07:10 +0300

    Create Main.java

---
+++ Lab7/Main.java
@@ -0,0 +1,45 @@
+package ru.mirea.lab7;
+
+import java.io.BufferedReader;
+
+import java.io.IOException;
+
+import java.io.InputStreamReader;
+
+import java.util.LinkedList;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        LinkedList<String> list = new LinkedList<>();
+
+        String s;
+
+        System.out.println("Enter empty line to stop adding items:");
+
+        while (!(s = reader.readLine()).isEmpty()) {
+
+            list.add(s);
+
+        }
+
+
+
+        System.out.println(list);
+
+        System.out.println("First: " + list.getFirst());
+
+        System.out.println("Last: " + list.getLast());
+
+        list.clear();
+
+        System.out.println("List after clearing: " + list.toString());
+
+    }
+
+}
