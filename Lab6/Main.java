d3359c4daed094841a2d633c45c6d6895e3e4c37
islam1999 <31537821+islam1999@users.noreply.github.com>
2018-11-19 11:05:16 +0300

    Update Main.java

--- Lab6/Main.java
+++ Lab6/Main.java
@@ -1,76 +1,85 @@
-// Не до конца сделан!!!!!!
-
-
-
-
-
-
 package ru.mirea.lab6;
+import javax.swing.*;
+import java.awt.*;
 
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
+public class Main {
 
-public class Main extends JFrame {
+    private static final int fontSize = 15;
+    public static void main(String[] args) {
 
-    public static void createGUI() {
+        JTextArea textArea = new JTextArea(10, 25);
 
-        JFrame frame = new JFrame("Test");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        Font font = new Font("Times New Roman", Font.PLAIN, 15);
+        JFrame frame = new JFrame("Text Changer");
 
         JMenuBar menuBar = new JMenuBar();
 
-        JMenu fileMenu = new JMenu("Цвет");
-            JMenuItem blueItem = new JMenuItem("Синий");
-                blueItem.setFont(font);
-                fileMenu.add(blueItem);
-            JMenuItem redItem = new JMenuItem("Красный");
-                redItem.setFont(font);
-                fileMenu.add(redItem);
-            JMenuItem blackItem = new JMenuItem("Черный");
-                blackItem.setFont(font);
-                fileMenu.add(blackItem);
-         fileMenu.addSeparator();
+        JMenu colorMenu = new JMenu("Color");
 
-        JMenu shriftMenu = new JMenu("Шрифт");
-            JMenuItem shrift1Item = new JMenuItem("Times New Roman");
-                shrift1Item.setFont(font);
-                shriftMenu.add(shrift1Item);
-            JMenuItem shrift2Item = new JMenuItem("MS Sans Serif");
-                shrift2Item.setFont(font);
-                shriftMenu.add(shrift2Item);
-            JMenuItem shrift3Item = new JMenuItem("Courier New");
-                shrift3Item.setFont(font);
-                shriftMenu.add(shrift3Item);
-        shriftMenu.addSeparator();
+        JMenu fontMenu = new JMenu("Font");
 
-        //shriftMenu.setFont(font);
-        shriftMenu.addSeparator();
 
-        menuBar.add(fileMenu);
-        menuBar.add(shriftMenu);
+
+        colorMenu.add(createColorChangingItem(textArea, "Blue", Color.BLUE));
+
+        colorMenu.add(createColorChangingItem(textArea, "Red", Color.RED));
+
+        colorMenu.add(createColorChangingItem(textArea, "Black", Color.BLACK));
+
+
+
+        fontMenu.add(createFontChangingItem(textArea, "Times New Roman"));
+
+        fontMenu.add(createFontChangingItem(textArea, "MS Sans Serif"));
+
+        fontMenu.add(createFontChangingItem(textArea, "Courier New"));
+
+
+
+        menuBar.add(colorMenu);
+
+        menuBar.add(fontMenu);
+
+
+
+        frame.add(textArea);
+
+
+
+        frame.setSize(500, 300);
+
+        frame.setResizable(false);
+
         frame.setJMenuBar(menuBar);
 
-        frame.setPreferredSize(new Dimension(500, 400));
-        frame.pack();
-        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                JFrame.setDefaultLookAndFeelDecorated(true);
-                createGUI();
-            }
-        });
+
+
+    private static JMenuItem createFontChangingItem(JTextArea area, String fontName) {
+
+        JMenuItem menuItem = new JMenuItem(fontName);
+
+        menuItem.addActionListener(e -> area.setFont(new Font(fontName, Font.PLAIN, fontSize)));
+
+        return menuItem;
+
     }
+
+
+
+    private static JMenuItem createColorChangingItem(JTextArea area, String cname, Color c) {
+
+        JMenuItem menuItem = new JMenuItem(cname);
+
+        menuItem.addActionListener(e -> area.setForeground(c));
+
+        return menuItem;
+
+    }
+
 }
